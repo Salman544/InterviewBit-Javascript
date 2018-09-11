387c276c421ca8cfdc74420494dfe2f9a49edc44
salman <salmansz500@icloud.com>
2018-09-12 00:45:22 +0500

    Model N,InMobi,Amazon

---
+++ Array/FirstMissingInteger.js
@@ -0,0 +1,15 @@
+function firstMissingInteger(a) {
+    let firstPositveInteger = Number.MIN_VALUE
+    let temp = a.sort().filter(e => e > 0)
+    if (temp.length === 1) { return 1 }
+    for(const [index, elm] of temp.entries()) {
+        if(elm !== index+1) {
+            firstPositveInteger = index+1
+            break
+        }
+    }
+    let bool = firstPositveInteger === Number.MIN_VALUE
+    return bool ? temp[temp.length -1] + 1 : firstPositveInteger
+}
+
+console.log(firstMissingInteger([-8, -7, -6]))
