91f5946f59a65b70995cf09a05fb18d30eb4cd5e
salman <salmansz500@icloud.com>
2018-09-12 00:08:35 +0500

    Google

--- Array/AddOneToNumber.js
+++ Array/AddOneToNumber.js
@@ -1,5 +1,5 @@
 
-// https://www.interviewbit.com/problems/add-one-to-number/
+//  https://www.interviewbit.com/problems/add-one-to-number/
 
 function addOneToNumber(a) {
     return String(Number(a.join('')) + 1).split('').map(e => Number(e)) 
